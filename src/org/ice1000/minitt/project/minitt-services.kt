ef0f0fdc7f778ac6e5f310c76f6499825a29e98f
ice1000 <ice1000kotlin@foxmail.com>
2019-03-13 20:55:30 -0400

    [ service ] Dinner is prepared, bathroom is prepared and .. I am prepared as well..

---
+++ src/org/ice1000/minitt/project/minitt-services.kt
@@ -0,0 +1,32 @@
+package org.ice1000.minitt.project
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.project.Project
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+data class MiniTTSettings(
+	var exePath: String = "minittc",
+	var version: String = "Unknown"
+)
+
+interface MiniTTProjectSettingsService {
+	val settings: MiniTTSettings
+}
+
+val Project.minittSettings: MiniTTProjectSettingsService
+	get() = ServiceManager.getService(this, MiniTTProjectSettingsService::class.java)
+
+@State(
+	name = "MiniTTProjectSettings",
+	storages = [Storage(file = "miniTTConfig.xml")])
+class MiniTTProjectSettingsServiceImpl :
+	MiniTTProjectSettingsService, PersistentStateComponent<MiniTTSettings> {
+	override val settings = MiniTTSettings()
+	override fun getState(): MiniTTSettings? = XmlSerializerUtil.createCopy(settings)
+	override fun loadState(state: MiniTTSettings) {
+		XmlSerializerUtil.copyBean(state, settings)
+	}
+}
