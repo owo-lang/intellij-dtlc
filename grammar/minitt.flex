aa5912a804d456093b28d5e0d7e0aafa1624c26d
ice1000 <ice1000kotlin@foxmail.com>
2019-03-28 16:27:39 -0400

    [ parser ] Fix some bugs

--- grammar/minitt.flex
+++ grammar/minitt.flex
@@ -32,17 +32,17 @@ IDENTIFIER=[a-z_'\\][a-zA-Z_0-9'\\]*
 CONSTRUCTOR=[A-Z][a-zA-Z_0-9'\\]*
 MULTIPLY = \*|\\times|\xd7|\u2716
 UNIVERSE = Type[0-9]*
+COMMENTS = --[^\n\r]*
 
 %%
 
 = { return EQ; }
-: { return COMMA; }
-, { return COLON; }
+: { return COLON; }
+, { return COMMA; }
 _ { return META_VAR; }
 ; { return SEMICOLON; }
 1 { return ONE_KEYWORD; }
 0 { return UNIT_KEYWORD; }
-\. { return DOT; }
 \| { return SEP; }
 \( { return LEFT_PAREN; }
 \{ { return LEFT_BRACE; }
@@ -50,6 +50,7 @@ _ { return META_VAR; }
 \} { return RIGHT_BRACE; }
 \.1 { return DOT_ONE; }
 \.2 { return DOT_TWO; }
+\.  { return DOT; }
 let { return LET_KEYWORD; }
 rec { return REC_KEYWORD; }
 sum { return SUM_KEYWORD; }
@@ -59,13 +60,14 @@ const { return CONST_KEYWORD; }
 ->|\u21d2 { return ARROW; }
 =>|\u2192 { return DOUBLE_ARROW; }
 {MULTIPLY} { return MUL; }
+{COMMENTS} { return LINE_COMMENT; }
 {UNIVERSE} { return TYPE_UNIVERSE; }
-{IDENTIFIER}  { return IDENTIFIER; }
-{CONSTRUCTOR} { return CONSTRUCTOR_NAME; }
-{WHITE_SPACE} { return WHITE_SPACE; }
-\\Pi|\u03A0    { return PI; }
-\\Sigma|\u03A3 { return SIGMA; }
+\\Pi|\u03A0     { return PI; }
+\\Sigma|\u03A3  { return SIGMA; }
 \\lambda|\u03BB { return LAMBDA; }
+{IDENTIFIER}    { return IDENTIFIER; }
+{CONSTRUCTOR}   { return CONSTRUCTOR_NAME; }
+{WHITE_SPACE}   { return WHITE_SPACE; }
 // \\forall|\u2200 { return FORALL; }
 
 [^] { return BAD_CHARACTER; }
--- src/org/ice1000/tt/psi/minitt.kt
+++ src/org/ice1000/tt/psi/minitt.kt
@@ -1,11 +1,20 @@
 package org.ice1000.tt.psi
 
+import com.intellij.lang.ASTNode
+import com.intellij.lang.ParserDefinition
+import com.intellij.lexer.FlexAdapter
 import com.intellij.openapi.project.Project
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.TokenType
+import com.intellij.psi.stubs.PsiFileStubImpl
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.IStubFileElementType
 import com.intellij.psi.tree.TokenSet
+import org.ice1000.tt.MiniTTFile
 import org.ice1000.tt.MiniTTLanguage
+import org.ice1000.tt.MiniTTLexer
 
 class MiniTTElementType(debugName: String) : IElementType(debugName, MiniTTLanguage.INSTANCE)
 
@@ -18,4 +27,23 @@ class MiniTTTokenType(debugName: String) : IElementType(debugName, MiniTTLanguag
 
 		fun fromText(text: String, project: Project) = PsiFileFactory.getInstance(project).createFileFromText(MiniTTLanguage.INSTANCE, text).firstChild
 	}
+}
+
+fun minittLexer() = FlexAdapter(MiniTTLexer())
+
+class MiniTTParserDefinition : ParserDefinition {
+	private companion object {
+		private val FILE = IStubFileElementType<PsiFileStubImpl<MiniTTFile>>(MiniTTLanguage.INSTANCE)
+	}
+
+	override fun createParser(project: Project?) = MiniTTParser()
+	override fun createLexer(project: Project?) = minittLexer()
+	override fun createElement(node: ASTNode?): PsiElement = MiniTTTypes.Factory.createElement(node)
+	override fun createFile(viewProvider: FileViewProvider) = MiniTTFile(viewProvider)
+	override fun getStringLiteralElements() = TokenSet.EMPTY
+	override fun getWhitespaceTokens() = MiniTTTokenType.WHITE_SPACE
+	override fun getCommentTokens() = MiniTTTokenType.COMMENTS
+	override fun getFileNodeType() = FILE
+	// TODO: replace after dropping support for 183
+	override fun spaceExistanceTypeBetweenTokens(left: ASTNode?, right: ASTNode?) = ParserDefinition.SpaceRequirements.MAY
 }
