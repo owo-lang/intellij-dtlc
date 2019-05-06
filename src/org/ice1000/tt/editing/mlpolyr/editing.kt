f38b1d224236b6a703e03d5c452e5372de400079
ice1000 <ice1000kotlin@foxmail.com>
2019-05-06 03:36:23 -0400

    [ mlpolyr ] Editing experience

---
+++ src/org/ice1000/tt/editing/mlpolyr/editing.kt
@@ -0,0 +1,39 @@
+package org.ice1000.tt.editing.mlpolyr
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+import org.ice1000.tt.MINI_TT_LINE_COMMENT
+import org.ice1000.tt.editing.DefaultCommenter
+import org.ice1000.tt.editing.DefaultFindUsagesProvider
+import org.ice1000.tt.psi.acore.acoreLexer
+import org.ice1000.tt.psi.mlpolyr.MLPolyRTokenType
+import org.ice1000.tt.psi.mlpolyr.MLPolyRTypes
+import org.ice1000.tt.psi.mlpolyr.mlpolyrLexer
+
+class MLPolyRCommenter : DefaultCommenter() {
+	override fun getBlockCommentPrefix() = "(*"
+	override fun getBlockCommentSuffix() = "*)"
+	override fun getLineCommentPrefix(): String? = null
+}
+
+class MLPolyRBraceMatcher : PairedBraceMatcher {
+	private companion object Pairs {
+		private val PAIRS = arrayOf(
+			BracePair(MLPolyRTypes.LP, MLPolyRTypes.RP, false),
+			BracePair(MLPolyRTypes.LSB, MLPolyRTypes.RSB, false),
+			BracePair(MLPolyRTypes.LCBB, MLPolyRTypes.RCBB, false),
+			BracePair(MLPolyRTypes.LCB, MLPolyRTypes.RCB, false))
+	}
+
+	override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+	override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+	override fun getPairs() = PAIRS
+}
+
+class MLPolyRFindUsagesProvider : DefaultFindUsagesProvider() {
+	override fun getWordsScanner() = DefaultWordsScanner(mlpolyrLexer(), MLPolyRTokenType.IDENTIFIERS, MLPolyRTokenType.COMMENTS, TokenSet.EMPTY)
+}
