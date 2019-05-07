8f2aba867d82c9cad94a8421bbd0e17439a30715
ice1000 <ice1000kotlin@foxmail.com>
2019-05-06 20:20:53 -0400

    [ mlpolyr ] Better icons

--- src/org/ice1000/tt/psi/mlpolyr/semantics.kt
+++ src/org/ice1000/tt/psi/mlpolyr/semantics.kt
@@ -5,9 +5,9 @@ import javax.swing.Icon
 
 enum class SymbolKind(val icon: Icon?) {
 	Function(PlatformIcons.FUNCTION_ICON),
-	RcFunction(PlatformIcons.PROPERTY_ICON),
+	RcFunction(PlatformIcons.FUNCTION_ICON),
 	Parameter(PlatformIcons.PARAMETER_ICON),
 	Variable(PlatformIcons.VARIABLE_ICON),
-	Pattern(PlatformIcons.FIELD_ICON),
+	Pattern(PlatformIcons.PROPERTY_ICON),
 	Unknown(null);
 }
