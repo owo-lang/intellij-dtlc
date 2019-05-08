b7b57a4363b0e747b93b09c39e30d5147c6bc44c
ice1000 <ice1000kotlin@foxmail.com>
2019-05-08 07:28:08 -0400

    [ bump ] Update version to 0.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val commitHash = kotlin.run {
 	output.trim()
 }
 
-val pluginComingVersion = "0.2.4"
+val pluginComingVersion = "0.2.5"
 val pluginVersion = if (isCI) "$pluginComingVersion-$commitHash" else pluginComingVersion
 val packageName = "org.ice1000.tt"
 
