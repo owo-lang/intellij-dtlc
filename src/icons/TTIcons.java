890b659671108ca8e58788a749db2b3d19f18ce0
ice1000 <ice1000kotlin@foxmail.com>
2019-05-10 13:50:07 -0400

    [ redprl ] File icons
    Optimize other file type icons

--- src/icons/TTIcons.java
+++ src/icons/TTIcons.java
@@ -10,13 +10,13 @@ public interface TTIcons {
 	@NotNull Icon MINI_TT = IconLoader.getIcon("/icons/minitt/minitt.png");
 	@NotNull Icon OWO_FILE = IconLoader.getIcon("/icons/owo_file.png");
 	@NotNull Icon AGDA_CORE_FILE = IconLoader.getIcon("/icons/mtt/mtt_file.png");
-	// TODO
 	@NotNull Icon AGDA_CORE = AGDA_CORE_FILE;
 	@NotNull Icon MLPOLYR_FILE = IconLoader.getIcon("/icons/mlpr/mlpr_file.png");
-	// TODO
 	@NotNull Icon MLPOLYR = MLPOLYR_FILE;
 	@NotNull Icon AGDA_FILE = IconLoader.getIcon("/icons/agda/agda_file.png");
 	@NotNull Icon AGDA = IconLoader.getIcon("/icons/agda/agda.png");
+	@NotNull Icon RED_PRL_FILE = IconLoader.getIcon("/icons/redprl/redprl_file.png");
+	@NotNull Icon RED_PRL = IconLoader.getIcon("/icons/redprl/redprl.png");
 	@NotNull Icon VOILE_FILE = IconLoader.getIcon("/icons/voile/voile_file.png");
 	@NotNull Icon VOILE = IconLoader.getIcon("/icons/voile/voile.png");
 }
