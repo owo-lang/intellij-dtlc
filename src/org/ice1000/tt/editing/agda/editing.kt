f0b7faf933c32276b0234071431b04fddca09b20
ice1000 <ice1000kotlin@foxmail.com>
2019-05-13 13:41:22 -0700

    [ agda ] Editing features

---
+++ src/org/ice1000/tt/editing/agda/editing.kt
@@ -0,0 +1,27 @@
+package org.ice1000.tt.editing.agda
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import org.ice1000.tt.editing.TTCommenter
+import org.ice1000.tt.psi.agda.AgdaTypes
+
+class AgdaCommenter : TTCommenter() {
+	override fun getBlockCommentPrefix() = "{-"
+	override fun getBlockCommentSuffix() = "-}"
+	override fun getLineCommentPrefix() = "--"
+}
+
+class AgdaBraceMatcher : PairedBraceMatcher {
+	private companion object Pairs {
+		private val PAIRS = arrayOf(
+			BracePair(AgdaTypes.OPEN_BRACE, AgdaTypes.CLOSE_BRACE, false),
+			BracePair(AgdaTypes.OPEN_IDIOM_BRACKET, AgdaTypes.CLOSE_IDIOM_BRACKET, false),
+			BracePair(AgdaTypes.OPEN_PAREN, AgdaTypes.CLOSE_PAREN, false))
+	}
+
+	override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+	override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+	override fun getPairs() = PAIRS
+}
