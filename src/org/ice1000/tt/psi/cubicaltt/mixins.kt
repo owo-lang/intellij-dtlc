399d3883189aae1eef4af29b273ca8be84b386be
ice1000 <ice1000kotlin@foxmail.com>
2019-05-17 21:01:32 -0700

    [ refactor ] Declarations

--- src/org/ice1000/tt/psi/cubicaltt/mixins.kt
+++ src/org/ice1000/tt/psi/cubicaltt/mixins.kt
@@ -3,11 +3,15 @@ package org.ice1000.tt.psi.cubicaltt
 import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.psi.ResolveState
 import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.stubs.IStubElementType
 import com.intellij.psi.tree.IElementType
+import com.intellij.util.IncorrectOperationException
+import icons.TTIcons
 import org.ice1000.tt.orTrue
+import org.ice1000.tt.psi.GeneralNameIdentifier
 
 abstract class CubicalTTModuleMixin : StubBasedPsiElementBase<CubicalTTModuleStub>, CubicalTTModule {
 	constructor(node: ASTNode) : super(node)
@@ -21,6 +25,27 @@ abstract class CubicalTTModuleMixin : StubBasedPsiElementBase<CubicalTTModuleStu
 	}
 
 	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		declList.asReversed().all { it.processDeclarations(processor, state, lastParent, place) }
+}
+
+abstract class CubicalTTDeclMixin : StubBasedPsiElementBase<CubicalTTDeclStub>, CubicalTTDecl, PsiNameIdentifierOwner {
+	constructor(node: ASTNode) : super(node)
+	constructor(stub: CubicalTTDeclStub, type: IStubElementType<*, *>) : super(stub, type)
+	constructor(stub: CubicalTTDeclStub, type: IElementType, node: ASTNode) : super(stub, type, node)
+
+	override fun getNameIdentifier() = findChildByClass(CubicalTTNameDecl::class.java)
+	override fun setName(newName: String): PsiElement {
+		CubicalTTTokenType.createNameDecl(newName, project)?.let { nameIdentifier?.replace(it) }
+		return this
+	}
+
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
 		nameIdentifier?.processDeclarations(processor, state, lastParent, place).orTrue()
 }
 
+abstract class CubicalTTNameDeclMixin(node: ASTNode) : GeneralNameIdentifier(node), CubicalTTNameDecl {
+	override fun getIcon(flags: Int) = TTIcons.CUBICAL_TT_FILE
+	@Throws(IncorrectOperationException::class)
+	override fun setName(newName: String) = replace(CubicalTTTokenType.createNameDecl(newName, project)
+		?: throw IncorrectOperationException("Invalid name: $newName"))
+}
