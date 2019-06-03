548de36c12dacc8560e551900f3dff7bbd8e33fe
ice1000 <ice1000kotlin@foxmail.com>
2019-06-03 14:17:37 -0700

    [ html export ] Improve generation algorithm

--- src/org/ice1000/tt/action/html-export.kt
+++ src/org/ice1000/tt/action/html-export.kt
@@ -5,10 +5,14 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.fileTypes.SyntaxHighlighter
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import com.intellij.openapi.progress.*
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiElement
-import com.intellij.util.containers.IntToIntSetMap
-import gnu.trove.TIntHashSet
-import kotlinx.html.*
+import gnu.trove.TIntObjectHashMap
+import kotlinx.html.PRE
+import kotlinx.html.a
+import kotlinx.html.classes
+import kotlinx.html.pre
 import kotlinx.html.stream.appendHTML
 import org.ice1000.tt.TTFile
 import org.ice1000.tt.psi.childrenWithLeaves
@@ -16,36 +20,60 @@ import org.ice1000.tt.psi.elementType
 import org.ice1000.tt.psi.startOffset
 import java.io.File
 
+private data class Info(
+	var classes: Set<String>? = null,
+	var href: String? = null
+)
+
 class HtmlExportAction : AnAction() {
 	lateinit var highlighter: SyntaxHighlighter
-	val idSet = TIntHashSet()
+	private val idMap = TIntObjectHashMap<Info>()
 
 	override fun update(e: AnActionEvent) {
 		e.presentation.isEnabledAndVisible = CommonDataKeys.PSI_FILE.getData(e.dataContext) is TTFile
 	}
 
 	override fun actionPerformed(e: AnActionEvent) {
-		idSet.clear()
+		idMap.clear()
 		val file = CommonDataKeys.PSI_FILE.getData(e.dataContext)?.takeIf { it is TTFile } ?: return
-		highlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(file.language, e.project, file.virtualFile)
+		val project = e.project
+		highlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(file.language, project, file.virtualFile)
 		val out = file.virtualFile.canonicalPath?.let { "$it.html" } ?: return
-		File(out).writer().use {
-			it.appendHTML().pre { traverse(file) }
-			it.flush()
-		}
-		idSet.clear()
+		ProgressManager.getInstance().run (object : Task.Backgroundable(project, "HTML Generation", true, PerformInBackgroundOption.ALWAYS_BACKGROUND) {
+			override fun run(indicator: ProgressIndicator) {
+				val ioFile = File(out)
+				ioFile.writer().use {
+					it.appendHTML().pre { traverse(file) }
+					it.flush()
+				}
+				VfsUtil.findFileByIoFile(ioFile, true)
+			}
+		})
+		idMap.clear()
 	}
 
-	private fun PRE.traverse(element: PsiElement): Unit = a {
+	private fun PRE.traverse(element: PsiElement) {
+		ProgressIndicatorProvider.checkCanceled()
 		val elementType = element.elementType
-		classes = highlighter.getTokenHighlights(elementType).map { it.externalName }.toSet()
+		val tokenHighlights = highlighter.getTokenHighlights(elementType)
 		val startOffset = element.startOffset
-		if (startOffset !in idSet) {
-			id = "$startOffset"
-			idSet.add(startOffset)
+		val info = idMap[startOffset] ?: Info().also { idMap.put(startOffset, it) }
+		if (tokenHighlights.isNotEmpty() && info.classes == null) info.classes = tokenHighlights.map { it.externalName }.toSet()
+		if (info.href == null) info.href = element.reference?.resolve()?.let { resolved ->
+			// Support cross-file reference?
+			if (resolved.containingFile == element.containingFile)
+				"#${resolved.startOffset}"
+			else null
 		}
+
 		if (element.firstChild != null)
 			element.childrenWithLeaves.forEach { this@traverse.traverse(it) }
-		else +element.text
+		else a {
+			val infoClasses = info.classes
+			if (infoClasses != null) classes = infoClasses
+			val infoHref = info.href
+			if (infoHref != null) href = infoHref
+			+element.text
+		}
 	}
 }
