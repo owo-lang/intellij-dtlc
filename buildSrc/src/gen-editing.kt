2caa041a0c73077b414bd5731361b949c42b102d
ice1000 <ice1000kotlin@foxmail.com>
2019-10-14 17:08:35 -0400

    [ #10 ] Generate Java for `ContextType`

--- buildSrc/src/gen-editing.kt
+++ buildSrc/src/gen-editing.kt
@@ -3,20 +3,29 @@ package org.ice1000.tt.gradle
 import org.intellij.lang.annotations.Language
 
 fun LanguageUtilityGenerationTask.editing(nickname: String) {
-	@Language("kotlin")
+	val className = "${languageName}DefaultContext"
+	@Language("JAVA")
 	val editing = """
-	package $basePackage.editing
+package $basePackage.editing;
 
-	import com.intellij.codeInsight.template.TemplateContextType
-	import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
-	import com.intellij.psi.PsiFile
-	import org.ice1000.tt.*
+import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.psi.PsiFile;
+import org.ice1000.tt.*;
+import org.jetbrains.annotations.NotNull;
 
-	class ${languageName}DefaultContext : TemplateContextType("${constantPrefix}_DEFAULT_CONTEXT_ID", ${constantPrefix}_LANGUAGE_NAME, TTParentContext::class.java) {
-		override fun isInContext(file: PsiFile, offset: Int) = file.fileType == ${languageName}FileType
+import static org.ice1000.tt.ConstantsKt.${constantPrefix}_LANGUAGE_NAME;
+
+class $className extends TemplateContextType {
+	public $className() {
+		super("${constantPrefix}_DEFAULT_CONTEXT_ID", ${constantPrefix}_LANGUAGE_NAME, TTParentContext.class);
 	}
-	"""
+
+	@Override
+	public boolean isInContext(@NotNull PsiFile file, int offset) {
+		return ${languageName}FileType.INSTANCE.equals(file.getFileType());
+	}
+}"""
 	outDir.resolve("editing")
 		.apply { mkdirs() }
-		.resolve("$nickname-generated.kt").writeText(editing)
+		.resolve("$className.java").writeText(editing)
 }
