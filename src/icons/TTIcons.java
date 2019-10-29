33e9142e4a1ae8619c1445c6039795a27e211edb
ice1000 <ice1000kotlin@foxmail.com>
2019-10-29 16:00:00 -0400

    [ icons ] Move some icons

--- src/icons/TTIcons.java
+++ src/icons/TTIcons.java
@@ -10,16 +10,16 @@ public interface TTIcons {
 	@NotNull Icon MINI_TT_FILE = IconLoader.getIcon("/icons/minitt/minitt_file.svg");
 	@NotNull Icon MINI_TT = IconLoader.getIcon("/icons/minitt/minitt.svg");
 	@NotNull Icon OWO_FILE = IconLoader.getIcon("/icons/owo_file.svg");
-	@NotNull Icon AGDA_CORE_FILE = IconLoader.getIcon("/icons/mtt/mtt_file.svg");
+	@NotNull Icon AGDA_CORE_FILE = IconLoader.getIcon("/icons/fileIcon/mtt_file.svg");
 	@NotNull Icon AGDA_CORE = AGDA_CORE_FILE;
 	// TODO: this is a placeholder
 	@NotNull Icon M_LANG_FILE = AGDA_CORE_FILE;
 	@NotNull Icon M_LANG = AGDA_CORE_FILE;
-	@NotNull Icon MLPOLYR_FILE = IconLoader.getIcon("/icons/mlpr/mlpr_file.svg");
+	@NotNull Icon MLPOLYR_FILE = IconLoader.getIcon("/icons/fileIcon/mlpr_file.svg");
 	@NotNull Icon MLPOLYR = MLPOLYR_FILE;
-	@NotNull Icon CUBICAL_TT_FILE = IconLoader.getIcon("/icons/ctt/ctt_file.svg");
+	@NotNull Icon CUBICAL_TT_FILE = IconLoader.getIcon("/icons/fileIcon/ctt_file.svg");
 	@NotNull Icon CUBICAL_TT = CUBICAL_TT_FILE;
-	@NotNull Icon YACC_TT_FILE = IconLoader.getIcon("/icons/ytt/ytt_file.svg");
+	@NotNull Icon YACC_TT_FILE = IconLoader.getIcon("/icons/fileIcon/ytt_file.svg");
 	@NotNull Icon YACC_TT = YACC_TT_FILE;
 	@NotNull Icon AGDA_FILE = IconLoader.getIcon("/icons/agda/agda_file.svg");
 	@NotNull Icon AGDA = IconLoader.getIcon("/icons/agda/agda.svg");
