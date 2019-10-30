33ef1fbd541c79276623edcb6ce9c59b7ba6f09b
ice1000 <ice1000kotlin@foxmail.com>
2019-10-30 00:48:43 -0400

    [ vitalyr ] Maybe this will work? Add quick fix for testing normalizer

---
+++ src/org/ice1000/tt/editing/vitalyr/annotator.kt
@@ -0,0 +1,50 @@
+package org.ice1000.tt.editing.vitalyr
+
+import com.intellij.codeInsight.intention.impl.BaseIntentionAction
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiUtil
+import org.ice1000.tt.TTBundle
+import org.ice1000.tt.VITALYR_LANGUAGE_NAME
+import org.ice1000.tt.psi.childrenWithLeaves
+import org.ice1000.tt.psi.vitalyr.VitalyRExpr
+import org.ice1000.tt.psi.vitalyr.VitalyRLambda
+import org.ice1000.tt.psi.vitalyr.VitalyRTokenType
+
+class VitalyRAnnotator : Annotator {
+	override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+		when (element) {
+			is VitalyRLambda -> lambda(element, holder)
+		}
+	}
+
+	private fun lambda(element: VitalyRLambda, holder: AnnotationHolder) {
+		if (PsiUtil.hasErrorElementChild(element)) return
+		val expr = element.expr ?: return
+		holder.createInfoAnnotation(expr, null).registerFix(BrutalEval(expr))
+	}
+}
+
+class BrutalEval(val expr: VitalyRExpr) : BaseIntentionAction() {
+	override fun getFamilyName() = VITALYR_LANGUAGE_NAME
+	override fun isAvailable(project: Project, editor: Editor?, psiFile: PsiFile?) = true
+	override fun getText() = TTBundle.message("vitalyr.lint.brutal-normalize", expr.text)
+	override operator fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+		if (PsiUtil.hasErrorElementChild(expr)) return
+		ApplicationManager.getApplication().runWriteAction {
+			val string = StringBuilder()
+			val ctx = file?.childrenWithLeaves.orEmpty()
+				.filterIsInstance<VitalyRLambda>()
+				.filterNot(PsiUtil::hasErrorElementChild)
+				.map { it.nameDecl!!.text to fromPsi(it.expr!!) }
+				.toMap().toMutableMap()
+			fromPsi(expr).bruteEval(ctx).toString(string, null)
+			VitalyRTokenType.createExpr(string.toString(), project)?.let(expr::replace)
+		}
+	}
+}
--- src/org/ice1000/tt/editing/vitalyr/normalize.kt
+++ src/org/ice1000/tt/editing/vitalyr/normalize.kt
@@ -1,26 +1,60 @@
 package org.ice1000.tt.editing.vitalyr
 
+import org.ice1000.tt.psi.vitalyr.VitalyRAppExpr
+import org.ice1000.tt.psi.vitalyr.VitalyRExpr
+import org.ice1000.tt.psi.vitalyr.VitalyRLamExpr
+import org.ice1000.tt.psi.vitalyr.VitalyRNameUsage
+import java.lang.StringBuilder
+
 typealias Ctx = MutableMap<String, Term>
 
 class EvaluationException(message: String) : Exception(message)
 
+fun fromPsi(element: VitalyRExpr): Term = when (element) {
+	is VitalyRLamExpr -> Abs(element.nameDecl!!.text, fromPsi(element.expr!!))
+	is VitalyRNameUsage -> Var(element.text)
+	is VitalyRAppExpr -> element.exprList.asSequence().map(::fromPsi).reduce(::App)
+	else -> throw EvaluationException("Bad expression: `${element.text}` of type ${element.javaClass}")
+}
+
 sealed class Term {
 	@Throws(EvaluationException::class)
 	abstract fun bruteEval(ctx: Ctx): Term
+
+	abstract fun toString(builder: StringBuilder, outer: Term?)
 }
 
 data class Var(val name: String) : Term() {
 	override fun bruteEval(ctx: Ctx) = ctx[name] ?: throw EvaluationException("Unresolved `$name`")
+	override fun toString(builder: StringBuilder, outer: Term?) {
+		builder.append(name)
+	}
 }
 
 data class Abs(val name: String, val body: Term) : Term() {
 	override fun bruteEval(ctx: Ctx) = this
+	override fun toString(builder: StringBuilder, outer: Term?) {
+		val paren = outer is App
+		if (paren) builder.append('(')
+		builder.append('\\').append(name).append(". ")
+		body.toString(builder, this)
+		if (paren) builder.append(')')
+	}
 }
 
 data class App(val f: Term, val a: Term) : Term() {
 	override fun bruteEval(ctx: Ctx) = when (val f = f.bruteEval(ctx)) {
 		is Abs -> f.body.bruteEval(ctx.also { it[f.name] = a })
 		else -> App(f, a.bruteEval(ctx))
+	}
+
+	override fun toString(builder: StringBuilder, outer: Term?) {
+		val paren = outer is App
+		if (paren) builder.append('(')
+		f.toString(builder, this)
+		builder.append(' ')
+		a.toString(builder, this)
+		if (paren) builder.append(')')
 	}
 }
 
