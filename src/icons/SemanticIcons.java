f2e394a9f03f660fdd659f4c790b2db180a4adc4
ice1000 <ice1000kotlin@foxmail.com>
2019-12-04 23:02:54 -0500

    [ narc ] AST types

--- src/icons/SemanticIcons.java
+++ src/icons/SemanticIcons.java
@@ -9,14 +9,15 @@ import javax.swing.*;
  * @author ice1000
  */
 public interface SemanticIcons {
-	@NotNull Icon BLUE_T = IconLoader.getIcon("/icons/ast/blue_t.png");
-	@NotNull Icon BLUE_C = IconLoader.getIcon("/icons/ast/blue_c.png");
-	@NotNull Icon BLUE_HOLE = IconLoader.getIcon("/icons/ast/blue_hole.png");
-	@NotNull Icon PURPLE_T = IconLoader.getIcon("/icons/ast/purple_t.png");
-	@NotNull Icon PURPLE_P = IconLoader.getIcon("/icons/ast/purple_p.png");
-	@NotNull Icon PINK_LAMBDA = IconLoader.getIcon("/icons/ast/pink_lambda.png");
-	@NotNull Icon ORANGE_F = IconLoader.getIcon("/icons/ast/orange_f.png");
-	@NotNull Icon ORANGE_LAMBDA = IconLoader.getIcon("/icons/ast/orange_lambda.png");
-	@NotNull Icon ORANGE_V = IconLoader.getIcon("/icons/ast/orange_v.png");
-	@NotNull Icon ORANGE_P = IconLoader.getIcon("/icons/ast/orange_p.png");
+	@NotNull Icon BLUE_T = IconLoader.getIcon("/icons/ast/blue_t.svg");
+	@NotNull Icon BLUE_C = IconLoader.getIcon("/icons/ast/blue_c.svg");
+	@NotNull Icon BLUE_HOLE = IconLoader.getIcon("/icons/ast/blue_hole.svg");
+	@NotNull Icon PURPLE_T = IconLoader.getIcon("/icons/ast/purple_t.svg");
+	@NotNull Icon PURPLE_P = IconLoader.getIcon("/icons/ast/purple_p.svg");
+	@NotNull Icon PINK_LAMBDA = IconLoader.getIcon("/icons/ast/pink_lambda.svg");
+	@NotNull Icon PINK_HOLE = IconLoader.getIcon("/icons/ast/pink_hole.svg");
+	@NotNull Icon ORANGE_F = IconLoader.getIcon("/icons/ast/orange_f.svg");
+	@NotNull Icon ORANGE_LAMBDA = IconLoader.getIcon("/icons/ast/orange_lambda.svg");
+	@NotNull Icon ORANGE_V = IconLoader.getIcon("/icons/ast/orange_v.svg");
+	@NotNull Icon ORANGE_P = IconLoader.getIcon("/icons/ast/orange_p.svg");
 }
--- src/org/ice1000/tt/psi/narc/semantics.kt
+++ src/org/ice1000/tt/psi/narc/semantics.kt
@@ -1,7 +1,24 @@
 package org.ice1000.tt.psi.narc
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.ResolveState
+import com.intellij.psi.scope.PsiScopeProcessor
 import org.ice1000.tt.psi.GeneralReference
+
+abstract class NarcDataMixin(node: ASTNode) : NarcDataGeneratedMixin(node), NarcData {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		constructorList.all {
+			it.processDeclarations(processor, state, lastParent, place)
+		} && super.processDeclarations(processor, state, lastParent, place)
+}
+
+abstract class NarcCodataMixin(node: ASTNode) : NarcCodataGeneratedMixin(node), NarcCodata {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		projectionList.all {
+			it.processDeclarations(processor, state, lastParent, place)
+		} && super.processDeclarations(processor, state, lastParent, place)
+}
 
 abstract class NarcNameUsageMixin(node: ASTNode) : GeneralReference(node), NarcNameUsage {
 }
