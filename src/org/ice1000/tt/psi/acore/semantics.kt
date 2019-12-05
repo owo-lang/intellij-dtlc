d81a00b52bf1de390d0b2a62ba92a884030359ed
ice1000 <ice1000kotlin@foxmail.com>
2019-12-04 22:42:03 -0500

    [ minitt, acore ] Merge `mixin` to `semantics`

--- src/org/ice1000/tt/psi/acore/semantics.kt
+++ src/org/ice1000/tt/psi/acore/semantics.kt
@@ -1,12 +1,19 @@
 package org.ice1000.tt.psi.acore
 
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveResult
+import com.intellij.psi.ResolveState
 import com.intellij.psi.impl.source.resolve.ResolveCache
+import com.intellij.psi.scope.PsiScopeProcessor
+import com.intellij.util.IncorrectOperationException
 import icons.TTIcons
+import org.ice1000.tt.orTrue
 import org.ice1000.tt.psi.*
+import org.ice1000.tt.psi.acore.impl.ACoreExpressionImpl
+import org.ice1000.tt.psi.acore.impl.ACorePatternImpl
 
 abstract class ACoreVariableMixin(node: ASTNode) : GeneralReference(node), ACoreVariable {
 	override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
@@ -28,4 +35,31 @@ abstract class ACoreVariableMixin(node: ASTNode) : GeneralReference(node), ACore
 			resolveWith(PatternResolveProcessor(ref.canonicalText), ref)
 		}
 	}
+}
+
+abstract class ACoreAtomPatternMixin(node: ASTNode) : ACorePatternImpl(node), ACoreAtomPattern {
+	override fun visit(visitor: (ACoreVariable) -> Boolean) =
+		variable?.let(visitor).orTrue() && pattern?.visit(visitor).orTrue()
+}
+
+abstract class ACorePairPatternMixin(node: ASTNode) : ACorePatternImpl(node), ACorePairPattern {
+	override fun visit(visitor: (ACoreVariable) -> Boolean) = patternList.all { it.visit(visitor) }
+}
+
+abstract class ACoreDeclarationExpressionMixin(node: ASTNode) : ASTWrapperPsiElement(node), ACoreDeclarationExpression {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		declaration.processDeclarations(processor, state, lastParent, place)
+}
+
+abstract class ACoreGeneralPattern(node: ASTNode) : GeneralNameIdentifier(node) {
+	override fun getIcon(flags: Int) = TTIcons.AGDA_CORE
+	@Throws(IncorrectOperationException::class)
+	override fun setName(newName: String) =
+		replace(ACoreTokenType.createPattern(newName, project)
+			?: invalidName(newName))
+}
+
+abstract class ACoreTypedAbstractionMixin(node: ASTNode) : ACoreExpressionImpl(node), ACoreTypedAbstraction {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		typedPattern.processDeclarations(processor, state, lastParent, place).orTrue()
 }
--- src/org/ice1000/tt/psi/minitt/semantics.kt
+++ src/org/ice1000/tt/psi/minitt/semantics.kt
@@ -1,13 +1,20 @@
 package org.ice1000.tt.psi.minitt
 
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveResult
+import com.intellij.psi.ResolveState
 import com.intellij.psi.impl.source.resolve.ResolveCache
+import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.IncorrectOperationException
 import icons.TTIcons
+import org.ice1000.tt.orTrue
 import org.ice1000.tt.psi.*
+import org.ice1000.tt.psi.minitt.impl.MiniTTExpressionImpl
+import org.ice1000.tt.psi.minitt.impl.MiniTTPatternImpl
 
 abstract class MiniTTVariableMixin(node: ASTNode) : GeneralReference(node), MiniTTVariable {
 	override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
@@ -33,4 +40,42 @@ abstract class MiniTTVariableMixin(node: ASTNode) : GeneralReference(node), Mini
 			}, ref)
 		}
 	}
+}
+
+abstract class MiniTTAtomPatternMixin(node: ASTNode) : MiniTTPatternImpl(node), MiniTTAtomPattern {
+	override fun visit(visitor: (MiniTTVariable) -> Boolean) =
+		variable?.let(visitor).orTrue() && pattern?.visit(visitor).orTrue()
+}
+
+abstract class MiniTTPairPatternMixin(node: ASTNode) : MiniTTPatternImpl(node), MiniTTPairPattern {
+	override fun visit(visitor: (MiniTTVariable) -> Boolean) = patternList.all { it.visit(visitor) }
+}
+
+abstract class MiniTTDeclarationExpressionMixin(node: ASTNode) : ASTWrapperPsiElement(node), MiniTTDeclarationExpression {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		declaration.processDeclarations(processor, state, lastParent, place)
+}
+
+abstract class MiniTTConstExpressionMixin(node: ASTNode) : ASTWrapperPsiElement(node), MiniTTConstExpression {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		constDeclaration.processDeclarations(processor, state, lastParent, place)
+}
+
+abstract class MiniTTGeneralPattern(node: ASTNode) : GeneralNameIdentifier(node) {
+	override fun getIcon(flags: Int) = TTIcons.MINI_TT
+	@Throws(IncorrectOperationException::class)
+	override fun setName(newName: String) =
+		replace(MiniTTTokenType.createPattern(newName, project) ?: invalidName(newName))
+}
+
+abstract class MiniTTTypedAbstractionMixin(node: ASTNode) : MiniTTExpressionImpl(node), MiniTTTypedAbstraction {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		typedPattern.processDeclarations(processor, state, lastParent, place)
+}
+
+abstract class MiniTTDeclarationMixin(node: ASTNode) : MiniTTDeclarationGeneratedMixin(node), MiniTTDeclaration {
+	override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
+		childrenRevWithLeaves.filterIsInstance<MiniTTPrefixParameter>().all {
+			it.typedPattern.processDeclarations(processor, state, lastParent, place)
+		} && super.processDeclarations(processor, state, lastParent, place)
 }
