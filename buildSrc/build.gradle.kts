e006bd6937c3892a1123410aadf43a1d76c63a50
Tesla Ice Zhang‮ <ice1000kotlin@foxmail.com>
2020-03-24 05:09:12 -0400

    [ gradle ] Upgrade buildSrc Kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,15 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "org.ice1000.tt.gradle"
 version = "114514"
 plugins {
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.serialization") version "1.3.61"
+	kotlin("jvm") version "1.3.70"
+	kotlin("plugin.serialization") version "1.3.70"
 }
 sourceSets { main { withConvention(KotlinSourceSet::class) { kotlin.srcDirs("src") } } }
 repositories { jcenter() }
 dependencies {
 	compile(kotlin("stdlib-jdk8"))
 	compile(kotlin("reflect"))
-	compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.14.0")
+	compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
 }
 val compilerArgs = listOf("-Xuse-experimental=kotlin.Experimental")
 tasks.withType<KotlinCompile> { kotlinOptions { freeCompilerArgs = compilerArgs } }
